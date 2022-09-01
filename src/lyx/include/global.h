4cc5b74cfb4fbb7b3bf4d5ec97d9bd015c285f3c
luoyuxiao <1324938402@qq.com>
2022-09-01 14:34:38 +0800

    feat: Add interupt handler

---
+++ src/lyx/include/global.h
@@ -0,0 +1,12 @@
+/* EXTERN is defined as extern except in global.c */
+#ifdef	GLOBAL_VARIABLES_HERE
+#undef	EXTERN
+#define	EXTERN
+#endif
+#include "type.h"
+#include "const.h"
+EXTERN	int		    disp_pos;
+EXTERN	u8		    gdt_ptr[6];	    /* 0~15:Limit  16~47:Base */
+EXTERN	DESCRIPTOR	gdt[GDT_SIZE];
+EXTERN	u8		    idt_ptr[6];	    /* 0~15:Limit  16~47:Base */
+EXTERN	GATE		idt[IDT_SIZE];
---
+++ src/lyx/include/string.h
@@ -0,0 +1,2 @@
+#include "const.h"
+PUBLIC	void*	memcpy(void* p_dst, void* p_src, int size);
